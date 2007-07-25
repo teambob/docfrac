3a2fed86b3ed50c6a2d83a0caac45da51458da67
andrewpunch <andrewpunch@426e91db-f250-454d-b98d-d7f335886676>
2007-07-25 11:58:02 +0000

    Initial MFC GUI
    git-svn-id: file:///home/andrew/Dropbox/data/svn/docfrac/trunk@52 426e91db-f250-454d-b98d-d7f335886676

---
+++ mfc-gui/Docfrac/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Docfrac.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DOCFRAC_DIALOG              102
+#define IDS_INPUT_FILENAME              102
+#define IDS_OUTPUT_FILENAME             103
+#define IDR_MAINMENU                    129
+#define IDI_APP_ICON                    131
+#define IDC_FILE_LIST                   1000
+#define ID_FILE_ADDFILES                32771
+#define ID_FILE_PROPERTIES              32772
+#define ID_FILE_CONVERT                 32773
+#define ID_FILE_                        32774
+#define ID_FILE_EXIT                    32775
+#define ID_EDIT_SELECTALL               32776
+#define ID_EDIT_REMOVE                  32777
+#define ID_EDIT_CLEARALL                32778
+#define ID_HELP_ABOUT                   32779
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ mfc-gui/Docfrac/tstring.h
@@ -0,0 +1,12 @@
+#ifndef __TSTRING_H__
+#define __TSTRING_H__
+
+
+#include <string>
+#include <vector>
+
+typedef std::basic_string<TCHAR> tstring;
+typedef std::vector<tstring> tstrings;
+
+
+#endif
